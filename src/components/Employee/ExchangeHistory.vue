d4c1700a66bb8cebd460a14f2180ee513b7fce2c
Vi Nguyen <vnguyen96@dxc.com>
2020-01-11 08:53:32 +0000

    the first time hanlde exchange history UI

---
+++ src/components/Employee/ExchangeHistory.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="form-group">
+        <div class="input-group date" id="datetimepicker1">
+          <input type="text" class="form-control" />
+          <span class="input-group-addon">
+            <span class="input-group-text">
+              <span class="fa fa-calendar"></span>
+            </span>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HeaderBar.vue
+++ src/views/HeaderBar.vue
@@ -11,7 +11,7 @@
           <b-navbar-nav>
             <b-nav-item :to="{name: 'createuser'}">{{ $t('createuser') }}</b-nav-item>
             <b-nav-item :to="{name: 'recharge'}">{{ $t('recharge') }}</b-nav-item>
-            <b-nav-item>{{ $t('exchangehistory') }}</b-nav-item>
+            <b-nav-item :to="{name: 'exchangehistory'}">{{ $t('exchangehistory') }}</b-nav-item>
             <b-nav-item :to="{name: 'login'}">{{ $t('logout') }}</b-nav-item>
           </b-navbar-nav>
         </b-navbar-nav>
