fcb5f744e92c7fc1ba93fdfa29b9bd984125efd4
Vi Nguyen <vnguyen96@dxc.com>
2020-02-15 08:23:05 +0000

    remove datepicker

--- src/components/User/TransactionManagement.vue
+++ src/components/User/TransactionManagement.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="col-lg-6 col-md-12 col-sm-12 margin-auto">
-    <div class="row">
-      <div class="col-md-6">
-        <date-picker v-model="date" :config="options"></date-picker>
-      </div>
-      <div class="col-md-6">
-        <date-picker v-model="date" :config="options"></date-picker>
-      </div>
-    </div>
-    <div class="row"></div>
     <b-tabs content-class="mt-3">
       <b-tab :title="$t('reciveMoney')" active>
         <recive-money />
@@ -27,25 +18,12 @@
 import ReciveMoney from '../Transaction/ReciveMoney.vue'
 import TransfersMoney from '../Transaction/TransfersMoney.vue'
 import PaymentDebtReminder from '../Transaction/PaymentDebtReminder.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import datePicker from 'vue-bootstrap-datetimepicker'
-import 'pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css'
 
 export default {
-  data () {
-    return {
-      date: new Date(),
-      options: {
-        format: 'DD/MM/YYYY',
-        useCurrent: false
-      }
-    }
-  },
   components: {
     ReciveMoney,
     TransfersMoney,
-    PaymentDebtReminder,
-    datePicker
+    PaymentDebtReminder
   }
-};
+}
 </script>
