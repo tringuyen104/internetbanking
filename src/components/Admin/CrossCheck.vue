4599128928e21cb84d8ab8da239577e4f7f0e11e
Vi Nguyen <vnguyen96@dxc.com>
2020-02-15 09:19:25 +0000

    fix UI for Cross check form

--- src/components/Admin/CrossCheck.vue
+++ src/components/Admin/CrossCheck.vue
@@ -3,27 +3,33 @@
     <h2 class="form-title">{{ $t('crossCheck') }}</h2>
     <div class="margin-auto">
     <div class="row">
-      <div class="col-md-4">
+      <div class="col-md-3">
         <label for="inputEmail4">{{ $t('fromdate') }}</label>
         <date-picker v-model="fromDate" :config="options"></date-picker>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-3">
         <label for="inputEmail4">{{ $t('todate') }}</label>
         <date-picker v-model="toDate" :config="options"></date-picker>
       </div>
-      <div class="col-md-2">
-        <div class="input-group">
+      <div class="col-md-4">
+        <!-- <div class="input-group">
           <b-nav-item-dropdown :text="$t('bank')" right>
           <b-dropdown-item>{{ $t('allbank') }}</b-dropdown-item>
           <b-dropdown-item>{{ $t('bank1') }}</b-dropdown-item>
           <b-dropdown-item>{{ $t('bank2') }}</b-dropdown-item>
           </b-nav-item-dropdown>
-        </div>
+        </div> -->
+        <div class="form-group">
+        <label for="account">{{ $t('bank') }}</label>
+        <select class="form-control" id="bank">
+          <option>{{ $t('allbank') }}</option>
+          <option>{{ $t('bank1') }}</option>
+          <option>{{ $t('bank2') }}</option>
+        </select>
+      </div>
       </div>
       <div class="col-md-2 submitbtn">
-        <div class="float-right">
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
       </div>
     </div>
     <br/>
