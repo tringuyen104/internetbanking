6edbf1b04190b157b5a61195d1d359346837f952
Vi Nguyen <vnguyen96@dxc.com>
2020-03-21 06:41:33 +0000

    validate change Pass

--- src/components/Form/ChangePass.vue
+++ src/components/Form/ChangePass.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="form-label">
+  <form class="form-label" @submit.prevent="changePassword">
     <h2 class="form-title">{{ $t('changePass') }}</h2>
       <div class="form-row">
         <div class="form-group col-md-12">
@@ -7,25 +7,34 @@
           <input
             type="password"
             class="form-control"
-            id="oldPassword"
+            name="oldPass"
+            v-validate="'required'"
+            v-model="model.oldPass"
           />
         </div>
+        <form-field-error :validation-errors="errors" :field="'oldPass'"></form-field-error>
         <div class="form-group col-md-12">
           <label for="inputPassword4">{{ $t('newPass') }}</label>
           <input
             type="password"
             class="form-control"
-            id="newPass"
+            name="newPass"
+            v-validate="'required'"
+            v-model="model.newPass"
           />
         </div>
+        <form-field-error :validation-errors="errors" :field="'newPass'"></form-field-error>
         <div class="form-group col-md-12">
           <label for="inputPassword4">{{ $t('confirmPass') }}</label>
           <input
             type="password"
             class="form-control"
-            id="confirmPass"
+            name="confirmPass"
+            v-validate="'required'"
+            v-model="model.confirmPass"
           />
         </div>
+        <form-field-error :validation-errors="errors" :field="'confirmPass'"></form-field-error>
         <div class="col-md-12 submitbtn">
         <div class="float-right">
           <button type="submit" class="btn btn-primary">Submit</button>
@@ -48,3 +57,28 @@
   margin-bottom: 2rem;
 }
 </style>
+<script>
+import FormFieldError from '../Errors/FormFieldError.vue'
+export default {
+  data () {
+    return {
+      model: {
+        oldPass: '',
+        newPass: '',
+        confirmPass: ''
+      }
+    }
+  },
+  components: { FormFieldError },
+  methods: {
+    changePassword () {
+      this.$validator.validateAll().then(valid => {
+        if (valid) {
+          alert('success')
+        }
+      })
+    }
+  }
+}
+
+</script>
